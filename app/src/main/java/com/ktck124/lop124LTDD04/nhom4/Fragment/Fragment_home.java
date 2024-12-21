1b21afb831d8b1dad2a92a392ed552316ed35421
Hieuiutatcamn <conghieu2k4@gmail.com>
2024-12-21 10:55:07 +0700

    Data

--- app/src/main/java/com/ktck124/lop124LTDD04/nhom4/Fragment/Fragment_home.java
+++ app/src/main/java/com/ktck124/lop124LTDD04/nhom4/Fragment/Fragment_home.java
@@ -4,24 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.ktck124.lop124LTDD04.nhom4.Model.User;
 import com.ktck124.lop124LTDD04.nhom4.R;
+import com.ktck124.lop124LTDD04.nhom4.SinhVienAdapter;
 
-
+import java.util.ArrayList;
 
 
 public class Fragment_home  extends Fragment {
 
     private View mview;
-
+    ListView lvSV;
+    ArrayList<User> arrayUsers;
+    SinhVienAdapter adapter;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mview = inflater.inflate(R.layout.fragment_home, container, false);
+        Anhxa();
+        adapter = new SinhVienAdapter(getContext(), R.layout.dongsv_activity, arrayUsers);
+        lvSV.setAdapter(adapter);
+
+
         return mview;
 
     }
+
+
+    private void Anhxa()
+    {
+        lvSV=(ListView) mview.findViewById(R.id.list_user);
+        arrayUsers= new ArrayList<>();
+        arrayUsers.add(new User("1", "nhat@gmail.com", "Nam", "123", "123", "22115053122127", "Phan Minh Nhat"));
+        arrayUsers.add(new User("1", "nhat@gmail.com", "Nam", "123", "123", "22115053122127", "Trần Công Hiếu"));
+        arrayUsers.add(new User("1", "nhat@gmail.com", "Nam", "123", "123", "22115053122127", "Đặng thanh mai"));
+    }
+
+
+
 }
